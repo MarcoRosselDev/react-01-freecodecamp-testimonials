32f6ae94e5b55a01993d158b99c40c4e3cf68b06
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-26 03:54:40 -0400

    graphic analysis

--- src/App.js
+++ src/App.js
@@ -4,30 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>
-          me gusta el te
-        </h1>
-        <div>
-          <ul className='ul'>a</ul>
-          <ul className='ul'>a</ul>
-          <ul className='ul'>a</ul>
-          <ul className='ul'>a</ul>
-          <ul className='ul'>a</ul>
-        </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          hir a react.com
-        </a>
-      </header>
+      
     </div>
   );
 }
--- src/component/Testimonio.js
+++ src/component/Testimonio.js
@@ -4,6 +4,11 @@ function Testimonio(){
   return(
     <div className="contenedor-testimonio">
       <img className="imagen-testimonio" src={require("../images/testimonio-emma.png")} alt="Foto de Emma"/>
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio">Emma Bostian en Suecia</p>
+        <p className="cargo-testimonio">Ingeniera de Software en Spotify</p>
+        <p className="texto-testimonio">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non repellendus cum porro libero accusantium soluta, cupiditate earum, modi rerum labore blanditiis quis expedita, similique perspiciatis aliquam praesentium excepturi doloribus? Maxime.</p>
+      </div>
     </div>
   );
 }
