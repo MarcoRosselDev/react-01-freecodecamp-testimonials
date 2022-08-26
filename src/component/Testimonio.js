082c2b2732c4f234f8282b221985a5682ccfb872
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-26 03:38:31 -0400

    delete unnecessary files

--- src/component/Testimonio.js
+++ src/component/Testimonio.js
@@ -3,6 +3,7 @@ import React from "react";
 function Testimonio(){
   return(
     <div className="contenedor-testimonio">
+      <img className="imagen-testimonio" src={require("../images/testimonio-emma.png")} alt="Foto de Emma"/>
     </div>
   );
 }
