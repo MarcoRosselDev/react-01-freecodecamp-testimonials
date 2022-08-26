64ea4ec6988c949bd22c3acc73ef877f816dfd08
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-26 07:02:27 -0400

    add shown and sarah

--- src/App.js
+++ src/App.js
@@ -14,6 +14,22 @@ function App() {
           empresa="Spotify"
           testimonio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae nostrum distinctio labore dignissimos reiciendis delectus eligendi quidem officia, ea voluptatem? Esse perferendis porro sunt recusandae error! Iste, nisi? Reiciendis, debitis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui odio corrupti aspernatur consectetur in laborum, quaerat explicabo blanditiis aliquid itaque nemo nulla perspiciatis voluptatem sit, ea, nesciunt recusandae quae excepturi."
         />
+        <Testimonio
+          nombre="Sarah Chima"
+          pais="Nigeria"
+          imagen="sarah"
+          cargo="Ingeniera de Software"
+          empresa="ChatDesk"
+          testimonio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae nostrum distinctio labore dignissimos reiciendis delectus eligendi quidem officia, ea voluptatem? Esse perferendis porro sunt recusandae error! Iste, nisi? Reiciendis, debitis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui odio corrupti aspernatur consectetur in laborum, quaerat explicabo blanditiis aliquid itaque nemo nulla perspiciatis voluptatem sit, ea, nesciunt recusandae quae excepturi."
+        />
+        <Testimonio
+          nombre="Shawn Wang"
+          pais="Singapur"
+          imagen="shawn"
+          cargo="Ingeniera de Software"
+          empresa="Amazon"
+          testimonio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae nostrum distinctio labore dignissimos reiciendis delectus eligendi quidem officia, ea voluptatem? Esse perferendis porro sunt recusandae error! Iste, nisi? Reiciendis, debitis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui odio corrupti aspernatur consectetur in laborum, quaerat explicabo blanditiis aliquid itaque nemo nulla perspiciatis voluptatem sit, ea, nesciunt recusandae quae excepturi."
+        />
       </div>
     </div>
   );
