6d255f8b32062fbcc8ca4fc7d8f4d319876d39ad
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-26 06:57:58 -0400

    apling props

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ function App() {
     <div className="App">
       <div className='contenedor-principal'>
         <h1>Esto es lo que dicen nuestros alumnos sobre FreeCodeCamp</h1>
-        <Testimonio/>
+        <Testimonio
+          nombre="Emma Bostian"
+          pais="Suecia"
+          imagen="emma"
+          cargo="Ingeniera de Software"
+          empresa="Spotify"
+          testimonio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae nostrum distinctio labore dignissimos reiciendis delectus eligendi quidem officia, ea voluptatem? Esse perferendis porro sunt recusandae error! Iste, nisi? Reiciendis, debitis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui odio corrupti aspernatur consectetur in laborum, quaerat explicabo blanditiis aliquid itaque nemo nulla perspiciatis voluptatem sit, ea, nesciunt recusandae quae excepturi."
+        />
       </div>
     </div>
   );
--- src/component/Testimonio.js
+++ src/component/Testimonio.js
@@ -1,14 +1,14 @@
 import React from "react";
 import '../stylesheets/Testimonio.css'
 
-function Testimonio(){
+function Testimonio(props){
   return(
     <div className="contenedor-testimonio">
-      <img className="imagen-testimonio" src={require("../images/testimonio-emma.png")} alt="Foto de Emma"/>
+      <img className="imagen-testimonio" src={require(`../images/testimonio-${props.imagen}.png`)} alt="Foto de Emma"/>
       <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio">Emma Bostian en Suecia</p>
-        <p className="cargo-testimonio">Ingeniera de Software en Spotify</p>
-        <p className="texto-testimonio">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non repellendus cum porro libero accusantium soluta, cupiditate earum, modi rerum labore blanditiis quis expedita, similique perspiciatis aliquam praesentium excepturi doloribus? Maxime.</p>
+        <p className="nombre-testimonio">{props.nombre} en {props.pais}</p>
+        <p className="cargo-testimonio">{props.cargo} en {props.empresa}</p>
+        <p className="texto-testimonio">{props.testimonio}</p>
       </div>
     </div>
   );
