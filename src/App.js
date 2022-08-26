9f01ebae29e80cfa9d0cca10c248b729e4525446
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-08-26 04:26:26 -0400

    styles app.css 2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import Testimonio from "./component/Testimonio.js"
 function App() {
   return (
     <div className="App">
-      <Testimonio/>
+      <div className='contenedor-principal'>
+        <h1>Esto es lo que dicen nuestros alumnos sobre FreeCodeCamp</h1>
+        <Testimonio/>
+      </div>
     </div>
   );
 }
